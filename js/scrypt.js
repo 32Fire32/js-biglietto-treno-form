c6a208abc64bfc445e22d0caf20c9586a5846dbf
32Fire32 <nicola.gianfelici@hotmail.it>
2022-10-21 15:37:17 +0200

    grafica e inizio logica

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,1 +1,50 @@
 'use strict'
+// RACCOLGO LE INFORMAZIONI
+let userName = document.getElementById("name").value;
+let userKm = document.getElementById("km").value;
+let userAge = document.getElementById("age").value;
+const btnGenerate = document.getElementById("generator");
+const display = document.querySelector(".ticket-result");
+const hideResult = document.getElementById("hide");
+totalPrice = userKm * 0.21;
+
+console.log(userName, userKm, userAge);
+
+
+btnGenerate.addEventListener('click', function(){
+    // if (userAge.value === "maggiorenne") {
+    //     totalPrice -= totalPrice * 0.2;
+    //     console.log(totalPrice);
+    // } else if (userAge.value === "over65") {
+    //     totalPrice -= totalPrice * 0.4;
+    //     console.log(totalPrice); 
+    // }    
+    // document.getElementById("price").innerHTML = totalPrice.toFixed(2);
+
+    document.getElementById("name-result").innerHTML = userName;
+})
+
+
+
+
+hideResult.addEventListener ('click', function (){
+    display.classList.add("ticket-none");
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
