65ab93252373b3e0b8c2e8b9844ee24c4b0febab
32Fire32 <nicola.gianfelici@hotmail.it>
2022-10-21 16:05:23 +0200

    aggiunge nome al biglietto e i km per fascia d'età

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,32 +1,44 @@
 'use strict'
-// RACCOLGO LE INFORMAZIONI
-let userName = document.getElementById("name").value;
-let userKm = document.getElementById("km").value;
-let userAge = document.getElementById("age").value;
+
 const btnGenerate = document.getElementById("generator");
 const display = document.querySelector(".ticket-result");
 const hideResult = document.getElementById("hide");
-totalPrice = userKm * 0.21;
-
-console.log(userName, userKm, userAge);
-
 
 btnGenerate.addEventListener('click', function(){
-    // if (userAge.value === "maggiorenne") {
-    //     totalPrice -= totalPrice * 0.2;
-    //     console.log(totalPrice);
-    // } else if (userAge.value === "over65") {
-    //     totalPrice -= totalPrice * 0.4;
-    //     console.log(totalPrice); 
-    // }    
-    // document.getElementById("price").innerHTML = totalPrice.toFixed(2);
 
-    document.getElementById("name-result").innerHTML = userName;
+    // AGGIUNGE NOME AL BIGLIETTO
+    var x = document.getElementById("name").value;
+    console.log(x);
+    document.getElementById("name-result").innerHTML = x;
+
+    
+    // CALCOLO DEL PREZZO IN BASE AI KM
+    let userKm = document.getElementById("km").value;
+    console.log(userKm);
+    let totalPrice = userKm * 0.21;
+    console.log(totalPrice);
+
+   
+    let userAge = document.getElementById("age").value;
+
+    if (userAge === "minorenne") {
+            totalPrice -= totalPrice * 0.2;
+            console.log(totalPrice);
+            document.getElementById("offer-result").innerHTML = "Biglietto Young";
+    } else if (userAge === "over65") {
+            totalPrice -= totalPrice * 0.4;
+            console.log(totalPrice); 
+            document.getElementById("offer-result").innerHTML = "Biglietto Over65";
+    }  else {
+            document.getElementById("price-result").innerHTML = totalPrice.toFixed(2);
+            document.getElementById("offer-result").innerHTML = "Biglietto Standard";
+    } 
+    
 })
 
 
 
-
+// NASCONDE IL BIGLIETTO IN CASO DI RESET
 hideResult.addEventListener ('click', function (){
     display.classList.add("ticket-none");
 })
